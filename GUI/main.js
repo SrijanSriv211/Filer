ae09e67eb11c6534c43b111b7bf4315ca26a5972
Light-Lens <QCoreNest@gmail.com>
2022-07-18 21:24:15 +0530

    Change the window size

--- GUI/main.js
+++ GUI/main.js
@@ -1,1 +1,9 @@
-console.warn("Hello world!");
+const {app, BrowserWindow} = require("electron");
+
+function CreateWindow()
+{
+    const Display = new BrowserWindow({ width: 400, height: 650 });
+    Display.loadFile("index.html");
+}
+
+app.whenReady().then(CreateWindow);
