886d29513635fe76a65587f6acc1dd8d792c12db
Light-Lens <QCoreNest@gmail.com>
2023-10-01 21:30:25 +0530

    initial states of encryption

--- src/main.py
+++ src/main.py
@@ -2,17 +2,18 @@ from vendor.AND.AND import AND
 from utils.shuffle import shuffle
 import string
 
-# rng = AND(p=0.578239823)
-rng = AND()
+rng = AND(p=0.578239823)
 r1 = rng.random()
 
-text = "Hello world!"
-text2 = "Hi Welcome!"
+txt = "Hello world!"
+txt2 = "Hi Welcome!"
 
-ascii_chars = list(string.ascii_letters + string.digits + string.punctuation + " ")
-random_nums = []
-for _ in range(len(ascii_chars)):
-    random_nums.append(rng.random())
+#* Works perfectly! DO NOT DELETE.
+# ascii_chars = [list(string.ascii_letters + string.digits + string.punctuation + " ")]*2
 
-print(ascii_chars[:10])
-print(shuffle(ascii_chars, random_nums)[:10])
+# random_nums = []
+# for _ in range(len(ascii_chars[1])):
+#     random_nums.append(rng.random())
+
+# randomly_packed_ascii_chars = list(zip(ascii_chars[0], shuffle(ascii_chars[1], random_nums)))[:10]
+# print(randomly_packed_ascii_chars)
