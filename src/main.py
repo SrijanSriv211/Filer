9da1549afddf5236d322408ea305da8f7cff59aa
Light-Lens <QCoreNest@gmail.com>
2023-10-02 17:04:27 +0530

    start the work on filer cli

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 from vendor.AND.AND import AND
-from utils.shuffle import shuffle
 import string
+
+def shuffle(lst, random_nums):
+    # Pair each string with a random number and sort by the random numbers
+    paired_lists = list(zip(random_nums, lst))
+    paired_lists.sort(key=lambda x: x[0])
+
+    # Extract the shuffled strings
+    return [x[1] for x in paired_lists]
 
 class Filer:
     def __init__(self, seed=-1):
