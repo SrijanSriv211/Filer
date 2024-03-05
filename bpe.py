8444a153704aade9e1a0dc336905ae86a9c8e1d3
Light-Lens <QCoreNest@gmail.com>
2024-03-05 06:08:21 +0530

    Update bpe.py

--- bpe.py
+++ bpe.py
@@ -1,7 +1,27 @@
 # Bye-Pair Encoding
+import string
+
 text = "aaabdaaabac"
 
-for maxlen in range(len(text)):
-    x = list(set([text[i:i+maxlen+1] for i in range(0, len(text), maxlen+1)]))
-    y = {i: text.count(i) for i in x}
-    print(f"{maxlen+1}.", max(y.values()), len(y))
+maxlen = 3
+n_gram = list(set([text[i:i + maxlen] for i in range(0, len(text), maxlen)]))
+
+vocab = {}
+num_idx = 0
+letters = string.ascii_letters
+for i, x in enumerate(n_gram):
+    if text.count(x) <= 1:
+        continue
+
+    if i + 1 % len(letters) == 0:
+        num_idx += 1
+
+    letter_idx = i % len(letters)
+    replacement = f"{num_idx}{letters[letter_idx]}"
+    vocab[x] = replacement
+
+for i, x in enumerate(n_gram):
+    if x in vocab:
+        n_gram[i] = vocab[x]
+
+print(n_gram)
