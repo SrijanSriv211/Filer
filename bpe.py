37c5d2e0f7f1bbffd9588e7741ef546866b8a3bf
Light-Lens <QCoreNest@gmail.com>
2024-03-09 02:38:40 +0530

    First iter of text compression using BPE

--- bpe.py
+++ bpe.py
@@ -1,8 +1,29 @@
 # Bye-Pair Encoding
 text = "aaabdaaabac"
+n = 0
+t = text[:]
 
-maxlen = 2
-x = set([text[i:i + maxlen] for i in range(0, len(text), maxlen)])
-y = {i: text.count(i) for i in x if text.count(i) > 1}
+for i in range(2):
+    z = {}
+    for maxlen in range(2, 10):
+        x = set([t[i:i + maxlen] for i in range(0, len(t), maxlen)])
+        y = {i: t.count(i) for i in x if t.count(i) > 1}
+        if len(y) == 0:
+            break
 
-print(y)
+        z[maxlen] = len(y)
+
+    best_len = max([k for k, v in z.items() if v == max(z.values())])
+
+    x = set([t[i:i + best_len] for i in range(0, len(t), best_len)])
+    y = {i: t.count(i) for i in x if t.count(i) > 1}
+
+    for i in y.keys():
+        new_t = t.replace(i, f"{n}")
+        if new_t != t:
+            print(f"{n} -> {i}")
+            t = new_t
+            n += 1
+
+    print(t)
+    print()
