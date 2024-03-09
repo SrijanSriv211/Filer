0c9d65476bccbce8b1613b8f532e0ab5add6e413
Light-Lens <QCoreNest@gmail.com>
2024-03-09 22:24:14 +0530

    A lot of problems

--- bpe.py
+++ bpe.py
@@ -2,40 +2,51 @@
 import string
 
 with open("e.txt", "r", encoding="utf-8") as f:
-    text = f.read()[:1000]
+    text = f.read()
 
-idx = 0
-text_copy = text[:]
+n_iters = 4
+idx, idx2 = 0, 0
+lst = []
 alphabets = string.ascii_letters
+for t in list(set(text.split())):
+    txt = t[:]
 
-for i in range(10):
-    z = {}
-    for maxlen in range(2, 10):
-        x = set([text_copy[i:i + maxlen] for i in range(0, len(text_copy), maxlen)])
-        y = {i: text_copy.count(i) for i in x if text_copy.count(i) > 1}
-        if not y:
+    for idx3 in range(n_iters):
+        z = {}
+        for maxlen in range(2, 10):
+            x = set([txt[i:i + maxlen] for i in range(0, len(txt), maxlen)])
+            y = {i: txt.count(i) for i in x if txt.count(i) > 1}
+            if not y:
+                break
+
+            z[maxlen] = len(y)
+
+        if not any(z.values()):
             break
 
-        z[maxlen] = len(y)
+        best_len = max([k for k, v in z.items() if v == max(z.values())], default=None)
 
-    if not any(z.values()):
-        break
+        if best_len is None:
+            break
 
-    best_len = max([k for k, v in z.items() if v == max(z.values())], default=None)
+        x = set([txt[i:i + best_len] for i in range(0, len(txt), best_len)])
+        y = {i: txt.count(i) for i in x if txt.count(i) > 1}
 
-    if best_len is None:
-        break
+        for i in y.keys():
+            replacement = f"{idx2}{alphabets[idx % len(alphabets)]}"
+            new_t = txt.replace(i, replacement)
 
-    x = set([text_copy[i:i + best_len] for i in range(0, len(text_copy), best_len)])
-    y = {i: text_copy.count(i) for i in x if text_copy.count(i) > 1}
+            if new_t != txt:
+                # print(f"{replacement} -> {i}")
+                txt = new_t
+                idx += 1
 
-    for i in y.keys():
-        replacement = alphabets[idx % len(alphabets)]
-        new_t = text_copy.replace(i, replacement)
-        if new_t != text_copy:
-            # print(f"{replacement} -> {i}")
-            text_copy = new_t
-            idx += 1
+                if idx3 == n_iters - 1:
+                    lst.append(txt)
 
-    print(text_copy)
-    print(f"{len(text_copy)/len(text)}")
+                if idx % len(alphabets) == 0:
+                    idx2 += 1
+
+print(lst)
+print(len(lst))
+print(len(list(set(text.split()))))
